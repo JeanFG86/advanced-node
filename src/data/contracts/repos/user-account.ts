778e92a0a72c111b31192fa28d7aa8fb904d7048
jeanfg86 <jeanfg86@gmail.com>
2022-04-05 19:52:37 -0300

    remove implementation details from service

--- src/data/contracts/repos/user-account.ts
+++ src/data/contracts/repos/user-account.ts
@@ -13,25 +13,14 @@ export namespace LoadUserAccountRepository{
   }
 }
 
-export interface CreateFacebookAccountRepository{
-  createFromFacebook: (params: CreateFacebookAccountRepository.Params) => Promise<void>
+export interface SaveFacebookAccountRepository{
+  saveWithFacebook: (params: SaveFacebookAccountRepository.Params) => Promise<void>
 }
 
-export namespace CreateFacebookAccountRepository{
+export namespace SaveFacebookAccountRepository{
   export type Params = {
+    id?: string
     email: string
-    name: string
-    facebookId: string
-  }
-}
-
-export interface UpdateacebookAccountRepository{
-  updateWithFacebook: (params: UpdateacebookAccountRepository.Params) => Promise<void>
-}
-
-export namespace UpdateacebookAccountRepository{
-  export type Params = {
-    id: string
     name: string
     facebookId: string
   }
