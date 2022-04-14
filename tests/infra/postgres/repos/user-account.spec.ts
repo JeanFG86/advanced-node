f6aa32bf718111816fc2399d670acf3753f35176
jeanfg86 <jeanfg86@gmail.com>
2022-04-14 20:34:18 -0300

    move inMemoryDb creation to its own file

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -1,17 +1,8 @@
-import { IBackup, IMemoryDb, newDb } from 'pg-mem'
+import { IBackup } from 'pg-mem'
 import { getConnection, getRepository, Repository } from 'typeorm'
 import { PgUserAccountRepository } from '@/infra/postgres/repos'
 import { PgUser } from '@/infra/postgres/entities'
-
-const makeFakeDb = async (entities?: any[]): Promise<IMemoryDb> => {
-  const db = newDb()
-  const connection = await db.adapters.createTypeormConnection({
-    type: 'postgres',
-    entities: entities ?? ['src/infra/postgres/entities/index.ts']
-  })
-  await connection.synchronize()
-  return db
-}
+import { makeFakeDb } from '@/tests/infra/postgres/mocks'
 
 describe('PgUserAccountRepository', () => {
   describe('load', () => {
@@ -19,7 +10,7 @@ describe('PgUserAccountRepository', () => {
     let pgUserRepo: Repository<PgUser>
     let backup: IBackup
     beforeAll(async () => {
-      const db = await makeFakeDb()
+      const db = await makeFakeDb([PgUser])
       backup = db.backup()
       pgUserRepo = getRepository(PgUser)
     })
