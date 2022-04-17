c0694ba4519ba0035f35902ef6785843ec9ecfde
jeanfg86 <jeanfg86@gmail.com>
2022-04-17 17:37:17 -0300

    apply unauthorized helper

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,4 +1,4 @@
-import { badRequest, HttpResponse } from '@/application/helpers'
+import { badRequest, HttpResponse, unauthorized } from '@/application/helpers'
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
 import { RequiredFieldError, ServerError } from '@/application/errors'
@@ -10,17 +10,14 @@ export class FacebookLoginController {
       if (httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
         return badRequest(new RequiredFieldError('token'))
       }
-      const result = await this.facebookAuthentication.perform({ token: httpRequest.token })
-      if (result instanceof AccessToken) {
+      const accessToken = await this.facebookAuthentication.perform({ token: httpRequest.token })
+      if (accessToken instanceof AccessToken) {
         return {
           statusCode: 200,
-          data: { accessToken: result.value }
+          data: { accessToken: accessToken.value }
         }
       } else {
-        return {
-          statusCode: 401,
-          data: result
-        }
+        return unauthorized()
       }
     } catch (error) {
       const e = new Error('infra_error')
