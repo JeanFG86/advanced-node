4ed49d8dbf2ee1420026f6023b120b07b2f31ea5
jeanfg86 <jeanfg86@gmail.com>
2022-04-22 14:45:56 -0300

    ensure FacebokApi return undefined when HttpGetClient throws

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  facebookApi: {
+    clientId: process.env.FB_CLIENT_ID ?? '562597475202505',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? '04cbe2523a2d95829f3ddae39b60f783'
+  }
+}
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,27 @@
-describe('', () => {
-  it('', () => {
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
 
+describe('Facebook Api Integration Tests', () => {
+  it('should return a Facebook User if token is valid', async () => {
+    const axiosCliente = new AxiosHttpClient()
+    const sut = new FacebookApi(axiosCliente, env.facebookApi.clientId, env.facebookApi.clientSecret)
+
+    const fbUser = await sut.loadUser({ token: 'EAAHZCre6pNckBAKbZAmhsvDdBuyvg2HZBKhTJht1v3cYdgKaIaY4oo4NL6mZCbx35rBEyV6qZCnMjB40ZAqeteuGOxmXBD3SytjiMFBLZCB3wDwXhG1ZA9neVUDWZCW1zLqcR5KdJGn4hZCHqfGT8CZAndsOE1dRMb4d57V5lJ1ZCGBrd7BeUW4J7kMCFJVc3qvJAupVZAFZAP4S0ouQZDZD' })
+
+    expect(fbUser).toEqual({
+      facebookId: '114689697883068',
+      email: 'jean_canmpim_teste@tfbnw.net',
+      name: 'Jean Teste'
+    })
+  })
+
+  it('should return a undefined if token is invalid', async () => {
+    const axiosCliente = new AxiosHttpClient()
+    const sut = new FacebookApi(axiosCliente, env.facebookApi.clientId, env.facebookApi.clientSecret)
+
+    const fbUser = await sut.loadUser({ token: 'invalid' })
+
+    expect(fbUser).toBeUndefined()
   })
 })
